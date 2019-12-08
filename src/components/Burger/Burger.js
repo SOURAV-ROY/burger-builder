a3b2425381d2289346b451003cb1bca6ce87d9f6
SOURAV ROY <souravroy.info13@gmail.com>
2019-12-08 23:39:40 +0600

    Awesome transformedIngredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,23 @@ import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+//  Awesome *********************************************************************************
     const transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey}/>;
             });
         });
-
+//  Awesome *********************************************************************************
+    
     return (
+
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+
         // <div className={classes.Burger}>
         //     <BurgerIngredient type="bread-top"/>
         //     <BurgerIngredient type="cheese"/>
@@ -20,12 +29,6 @@ const burger = (props) => {
         /*    <BurgerIngredient type="cheese"/>*/
         /*    <BurgerIngredient type="bread-bottom"/>*/
         /*</div>*/
-
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom"/>
-        </div>
     );
 };
 
