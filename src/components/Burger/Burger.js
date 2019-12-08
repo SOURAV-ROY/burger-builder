81bfde2a3056b11f099bb08a45a34c3d3e9e5a6b
SOURAV ROY <souravroy.info13@gmail.com>
2019-12-08 23:35:38 +0600

    transformedIngredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,27 @@ import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>;
+            });
+        });
+
     return (
+        // <div className={classes.Burger}>
+        //     <BurgerIngredient type="bread-top"/>
+        //     <BurgerIngredient type="cheese"/>
+        //     <BurgerIngredient type="bacon"/>
+        //     <BurgerIngredient type="salad"/>
+        /*    <BurgerIngredient type="meat"/>*/
+        /*    <BurgerIngredient type="cheese"/>*/
+        /*    <BurgerIngredient type="bread-bottom"/>*/
+        /*</div>*/
+
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,26 @@ import React, {Component} from 'react';
 import Aux from "../../hoc/ReactAux";
 import Burger from "../../components/Burger/Burger";
 
-class BurgerBuilder extends Component{
+class BurgerBuilder extends Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state={};
+    // }
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    };
+
     render() {
-        return(
+        return (
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
+
                 <div>Burger Builder</div>
             </Aux>
         );
