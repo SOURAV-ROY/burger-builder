758a5bb1bac9be583c2f8c7b0e0860ad60e9eb27
SOURAV ROY <souravroy.info13@gmail.com>
2019-12-12 23:51:45 +0600

    SideDrawer Added With Mobile View

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,33 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+import React from 'react';
 import Aux from "../../hoc/ReactAux";
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-class layout extends Component {
-    render() {
-        return (
-            <Aux>
-                {/*<div className={classes.Header}>Toolbar , Sidebar</div>*/}
-                <Toolbar/>
-                <main className={classes.Content}>
-                    {this.props.children}
-                </main>
-            </Aux>
-        );
-    }
-}
-
+const layout = (props) => (
+    <Aux>
+        {/*<div className={classes.Header}>Toolbar , Sidebar</div>*/}
+        <Toolbar/>
+        <SideDrawer/>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+);
 
 export default layout;
+
+// class layout extends Component {
+//     render() {
+//         return (
+//             <Aux>
+//                 {/*<div className={classes.Header}>Toolbar , Sidebar</div>*/}
+//                 <Toolbar/>
+//                 <main className={classes.Content}>
+//                     {this.props.children}
+//                 </main>
+//             </Aux>
+//         );
+//     }
+// }
