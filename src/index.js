09dbd87339c21fd80356bdde49c16deb1a02f087
SOURAV ROY <souravroy.info13@gmail.com>
2019-12-16 14:06:45 +0600

    Axios Install

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Layout from "./components/Layout/Layout";
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<Layout />, document.getElementById('layout'));
-// ReactDOM.render(<App />, document.getElementById('layout'));
+
 registerServiceWorker();
