ba531037488481803bb38fd199c9d618a4d67432
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-06 00:49:13 +0600

    Style With BDT update

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -30,7 +30,7 @@ const order = (props) => {
     return (
         <div className={classes.Order}>
             <p>Ingredients: {ingredientOutput}</p>
-            <p>Price: <strong>BDT = {Number.parseFloat(props.price).toFixed(2)} &#2547;</strong></p>
+            <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)} &#2547;</strong></p>
         </div>
     );
 };
