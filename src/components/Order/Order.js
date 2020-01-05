19290aec922de6254ed38cbfaa5406af5526bde6
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-06 00:20:49 +0600

    Order Update With Style

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -15,20 +15,21 @@ const order = (props) => {
 
     const ingredientOutput = ingredients.map(ig => {
         return <span
-            style={{
-                textTransform: 'capitalize',
-                display: 'inline-block',
-                margin: '0 8px',
-                border: '1px solid #ccc',
-                padding: '5px'
-            }}
+            className={classes.Ingredients}
+            // style={{
+            //     textTransform: 'capitalize',
+            //     display: 'inline-block',
+            //     margin: '0 8px',
+            //     border: '1px solid #ccc',
+            //     padding: '5px'
+            // }}
             key={ig.name}>{ig.name} ({ig.amount})</span>;
     });
 
     return (
         <div className={classes.Order}>
             <p>Ingredients: {ingredientOutput}</p>
-            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+            <p>Price: <strong>BDT {Number.parseFloat(props.price).toFixed(2)}</strong></p>
         </div>
     );
 };
