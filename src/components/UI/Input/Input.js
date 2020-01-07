063f237ddabd93c3e7ffc3aa9447a08827d7b3a4
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-08 01:17:31 +0600

    Handling User Input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,15 +5,43 @@ const input = (props) => {
 
     let inputElement = null;
 
-    switch (props.inputtype) {
+    switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props}/>;
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}
+            />;
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props}/>;
+            inputElement = <textarea
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}
+            />;
+            break;
+        case ('select'):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    {...props.elementConfig}
+                    value={props.value}
+                onChange={props.changed}>
+                    {props.elementConfig.option.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+
+                </select>
+            );
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props}/>
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig} value={props.value}/>
     }
 
     return (
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
@@ -7,62 +7,151 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
-        },
-        loading: false
+        // name: '',
+        // email: '',
+        // address: {
+        //     street: '',
+        //     postalCode: ''
+        // },
 
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP CODE'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliverMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    option: [
+                        {value: 'fastest', displayValue: 'FASTEST'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            }
+        },
+
+        loading: false
     };
+
 
     orderHandler = (event) => {
         event.preventDefault();
-        this.setState( { loading: true } );
+        this.setState({loading: true});
         const order = {
             ingredients: this.props.ingredients,
             // On production, calculate this on the server
             // so users cannot manipulate it before it's
             // sent to the server.
             price: this.props.price,
-            customer: {
-                name: 'SOURAV ROY',
-                address: {
-                    street: 'KHILKHET',
-                    zipCode: '1229',
-                    country: 'DHAKA'
-                },
-                email: 'sourav@admin.com'
-            },
-            deliverMethod: 'fastest'
+
+            // customer: {
+            //     name: 'SOURAV ROY',
+            //     address: {
+            //         street: 'KHILKHET',
+            //         zipCode: '1229',
+            //         country: 'DHAKA'
+            //     },
+            //     email: 'sourav@admin.com'
+            // },
+            // deliverMethod: 'fastest'
         };
 
         // orders will be the name of the node created
         // in firebase
         axios.post('/orders.json', order)
             .then(response => {
-                this.setState({ loading: false });
+                this.setState({loading: false});
                 this.props.history.push('/');
             })
             .catch(error => {
-                this.setState({ loading: false });
+                this.setState({loading: false});
             });
     };
 
-    render () {
+    inputChangedHandler = (event, inputIdentifier) => {
+        // console.log(event.target.value);
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});
+    };
+
+    render() {
+
+        const formElementArray = [];
+        for (let key in this.state.orderForm) {
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name"/>
-                <Input inputtype="input" type="email" name="email" placeholder="Your Email"/>
-                <Input inputtype="input" type="text" name="street" placeholder="Street"/>
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code"/>
+
+                {/*<Input elementType="..." elementConfig="..." value="..."/>*/}
+
+                {formElementArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}
+                    />
+                ))}
+                {/*<Input inputtype="input" type="text" name="name" placeholder="Your Name"/>*/}
+                {/*<Input inputtype="input" type="email" name="email" placeholder="Your Email"/>*/}
+                {/*<Input inputtype="input" type="text" name="street" placeholder="Street"/>*/}
+                {/*<Input inputtype="input" type="text" name="postal" placeholder="Postal Code"/>*/}
 
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
         if (this.state.loading) {
-            form = <Spinner />;
+            form = <Spinner/>;
 
         }
 
