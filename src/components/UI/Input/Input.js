7ce216386cfbf03e8216525aa0422d886e6eb239
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-09 14:23:18 +0600

    Adding Custom Form Validation

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,11 +4,16 @@ import classes from './Input.css';
 const input = (props) => {
 
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate){
+        inputClasses.push(classes.Invalid);
+    }
 
     switch (props.elementType) {
         case ('input'):
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value}
                 onChange={props.changed}
@@ -16,7 +21,7 @@ const input = (props) => {
             break;
         case ('textarea'):
             inputElement = <textarea
-                className={classes.InputElement}
+                className={inputClasses}
                 {...props.elementConfig}
                 value={props.value}
                 onChange={props.changed}
@@ -25,7 +30,7 @@ const input = (props) => {
         case ('select'):
             inputElement = (
                 <select
-                    className={classes.InputElement}
+                    className={inputClasses}
                     {...props.elementConfig}
                     value={props.value}
                 onChange={props.changed}>
@@ -40,7 +45,7 @@ const input = (props) => {
             break;
         default:
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses}
                 {...props.elementConfig} value={props.value}/>
     }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -47,7 +47,9 @@ class ContactData extends Component {
                 },
                 value: '',
                 validation: {
-                    required: true
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
                 },
                 valid: false
             },
@@ -139,14 +141,21 @@ class ContactData extends Component {
 
     checkValidity(value, rules) {
         let isValid = false;
+
         if (rules.required) {
             isValid = value.trim() !== '';
+        }
+        if (rules.minLength){
+            isValid= value.length >= rules.minLength;
+        }
+        if (rules.maxLength){
+            isValid= value.length <= rules.maxLength;
         }
         return isValid;
     }
 
     inputChangedHandler = (event, inputIdentifier) => {
-        console.log(event.target.value);
+        // console.log(event.target.value);
 
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -184,6 +193,8 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}
                     />
                 ))}
