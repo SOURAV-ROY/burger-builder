f55c5845f77cf08326ea1fdd05abe4eb0b31433a
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-13 21:37:18 +0600

    store by redux

---
+++ src/store/reducer.js
@@ -0,0 +1,11 @@
+import * as actionType from './actions';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 20
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+export default reducer;
