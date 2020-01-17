31d702fb29ee98c793e03a1622f2d347ee916796
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-17 15:26:20 +0600

    Actions & Reducer Fila Dir Change :)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from "../../hoc/WithErrorHandler/WithErrorHandler";
 import axios from '../../axios-orders';
-import * as actionTypes from '../../store/actions';
+import * as actionTypes from '../../store/actions/actions';
 
 // const INGREDIENT_PRICE = {
 //     salad: 25.33,
--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import {createStore} from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from "./store/reducer";
+import reducer from "./store/reducer/reducer";
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store={store}>
