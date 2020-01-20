c540c709eb1c239c8110c8df9c1a1acbb3ca103d
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-20 15:07:22 +0600

    Adding An Auth Form With Validation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from './containers/Checkout/Checkout';
 import Orders from "./containers/Orders/Orders";
+import Auth from "./containers/Auth/Auth";
 
 class App extends Component {
 
@@ -26,7 +27,8 @@ class App extends Component {
                         {/*{this.state.show ? <BurgerBuilder/> : null}*/}
 
                         <Route path="/checkout" component={Checkout}/>
-                        <Route path="/orders" component={Orders} />
+                        <Route path="/orders" component={Orders}/>
+                        <Route path="/auth" component={Auth}/>
                         <Route path="/" component={BurgerBuilder}/>
 
                     </Switch>
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,118 @@
+import React, {Component} from 'react';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import classes from './Auth.css';
+
+class Auth extends Component {
+    state = {
+
+        controls: {
+
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+    };
+
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if (!rules) {
+            return true;
+        }
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        if (rules.isEmail) {
+
+            // eslint-disable-next-line no-useless-escape
+            const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            isValid = pattern.test(value) && isValid;
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        };
+        this.setState({controls: updatedControls});
+    };
+
+    render() {
+
+        const formElementArray = [];
+        for (let key in this.state.controls) {
+            formElementArray.push({
+                id: key,
+                config: this.state.controls[key]
+            });
+        }
+
+        const form = formElementArray.map(formElement => (
+            <Input
+                key={formElement.id}
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}
+                invalid={!formElement.config.valid}
+                shouldValidate={formElement.config.validation}
+                touched={formElement.config.touched}
+                changed={(event) => this.inputChangedHandler(event, formElement.id)}
+            />
+
+        ));
+
+        return (
+            <div className={classes.Auth}>
+                <form>
+                    {form}
+                    <Button btnType="Success">SUBMIT</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Auth;
