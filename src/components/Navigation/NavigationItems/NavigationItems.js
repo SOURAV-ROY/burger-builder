ccf3235899d1077f9d3f4a044cce9d8a145621fe
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-22 17:56:11 +0600

    Login And Show Order Option Without NOT😜

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,10 @@ const navigationItems = (props) => (
 
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-        <NavigationItem link="/orders">Orders</NavigationItem>
+        {props.isAuthenticated
+            ? <NavigationItem link="/orders">Orders</NavigationItem>
+            : null
+        }
 
         {!props.isAuthenticated
             ? <NavigationItem link="/auth">LogIn</NavigationItem>
