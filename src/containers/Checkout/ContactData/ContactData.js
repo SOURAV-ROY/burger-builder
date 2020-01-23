fb580aab267842053929a49b232d53dc6be683e3
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-23 16:24:39 +0600

    UserId Catch After Submit Order

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -126,6 +126,7 @@ class ContactData extends Component {
             // sent to the server.
             price: this.props.price,
             orderData: formData,
+            userId: this.props.userId
         };
 
         // this.props.onOrderBurger(order);
@@ -246,7 +247,8 @@ const mapStateToProps = state => {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 };
 
