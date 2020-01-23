d6432c792baf49d842ba7a76af8199bc96f3d40a
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-23 17:20:58 +0600

    Displaying User Specific Orders👍

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -16,7 +16,7 @@ class Orders extends Component {
 
     componentDidMount() {
 
-        this.props.onFetchOrders(this.props.token);
+        this.props.onFetchOrders(this.props.token, this.props.userId);
 
         // axios.get('/orders.json')
         //     .then(res => {
@@ -76,13 +76,14 @@ const mapStateToProps = state => {
     return {
         orders: state.order.orders,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
+        onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
     };
 };
 
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -66,13 +66,18 @@ export const fetchOrdersStart = () => {
     };
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         // return (dispatch, getState) => {
         dispatch(fetchOrdersStart());
 
         // axios.get('/orders.json')
-        axios.get('/orders.json?auth=' + token)
+
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+
+        // axios.get('/orders.json?auth=' + token)
+        axios.get('/orders.json' + queryParams)
+
             .then(res => {
                 // console.log(res.data);
                 const fetchedOrders = [];
@@ -87,6 +92,7 @@ export const fetchOrders = (token) => {
                 // this.setState({loading: false, orders: fetchedOrders});
 
             })
+
             .catch(err => {
                 dispatch(fetchOrdersFail(err));
                 // this.setState({loading: false});
