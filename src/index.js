26da6f9255c8bba7650b8c848e4638e2c144b879
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-24 13:53:20 +0600

    Using Environment Variables

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ import burgerBuilderReducer from "./store/reducer/burgerBuilder";
 import orderReducer from './store/reducer/order';
 import authReducer from './store/reducer/auth';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
