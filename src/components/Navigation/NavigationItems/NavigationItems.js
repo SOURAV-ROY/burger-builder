f49b5d2b2d2d7905468f91099a58103c896352d0
SOURAV ROY <souravroy.info13@gmail.com>
2020-01-26 12:23:35 +0600

    Enzyme Tester added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,9 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => (
 
     <ul className={classes.NavigationItems}>
+
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+
         {props.isAuthenticated
             ? <NavigationItem link="/orders">Orders</NavigationItem>
             : null
